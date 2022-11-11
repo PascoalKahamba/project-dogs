7c7bb53c5dfa2914085dabb76ac8789455e623fe
pascoalkahamba <pascoalkahamba25@gmail.com>
2022-11-11 12:31:38 +0100

    criei o componente PhotoPost

---
+++ src/api/edpoints/PhotoPost.jsx
@@ -0,0 +1,60 @@
+import React, { useState } from "react";
+
+const PhotoPost = () => {
+  const [token, setToken] = useState("");
+  const [nome, setNome] = useState("");
+  const [idade, setIdade] = useState("");
+  const [peso, setPeso] = useState("");
+  const [img, setImg] = useState("");
+
+  function handleSubmit(event) {
+    event.preventDefault();
+    fetch("https://dogsapi.origamid.dev/json/api/user", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: {},
+    })
+      .then((response) => {
+        console.log(response);
+        return response.json();
+      })
+      .then((json) => {
+        console.log(json);
+        return json;
+      });
+  }
+  return (
+    <form onSubmit={handleSubmit}>
+      <input
+        type="text"
+        placeholder="username"
+        value={token}
+        onChange={({ target }) => setToken(target.value)}
+      />
+      <input
+        type="text"
+        placeholder="nome"
+        value={nome}
+        onChange={({ target }) => setNome(target.value)}
+      />
+      <input
+        type="text"
+        placeholder="peso"
+        value={peso}
+        onChange={({ target }) => setPeso(target.value)}
+      />
+      <input
+        type="text"
+        placeholder="idade"
+        value={idade}
+        onChange={({ target }) => setIdade(target.value)}
+      />
+      <input type="file" onChange={({ target }) => setImg(target.files[0])} />
+      <button>enviar</button>
+    </form>
+  );
+};
+
+export default PhotoPost;
