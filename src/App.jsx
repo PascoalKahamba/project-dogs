3781f059037b862dc5cc751c96f7beab3acf4bde
pascoalkahamba <pascoalkahamba25@gmail.com>
2022-11-11 17:11:31 +0100

    criei a Home

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import { BrowserRouter, Route, Routes } from "react-router-dom";
 import "./App.css";
 import Footer from "./Components/Footer";
 import Header from "./Components/Header";
+import Home from "./Components/Home";
 
 const App = () => {
   return (
