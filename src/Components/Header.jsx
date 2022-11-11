2f78e7307a278229d9ea25ba6c59d36fccb85720
pascoalkahamba <pascoalkahamba25@gmail.com>
2022-11-11 17:22:37 +0100

    Criei o componente de Login

--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -1,8 +1,16 @@
 import React from "react";
+import { Link } from "react-router-dom";
 import styles from "./Header.module.css";
 
 const Header = () => {
-  return <div style={styles.header}>Header</div>;
+  return (
+    <div style={styles.header}>
+      <nav>
+        <Link to="">Home</Link>
+        <Link to="/login">Login | Criar</Link>
+      </nav>
+    </div>
+  );
 };
 
 export default Header;
