ac0d405befd623bd1d7ebb5d59b65e1720034c6c
pascoalkahamba <pascoalkahamba25@gmail.com>
2022-11-11 11:19:37 +0100

    criei o componente TokenPost

--- src/api/Api.jsx
+++ src/api/Api.jsx
@@ -1,11 +1,14 @@
 import React from "react";
+import TokenPost from "./edpoints/TokenPost";
 import UserPost from "./edpoints/UserPost";
 
 const Api = () => {
   return (
     <div>
-      <h2>User Post</h2>
+      <h2>USER POST</h2>
       <UserPost />
+      <h2>TOKEN POST</h2>
+      <TokenPost />
     </div>
   );
 };
