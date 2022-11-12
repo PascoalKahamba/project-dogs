326ad78d7125851a5a01a15c77ea6555e32257d4
pascoalkahamba <pascoalkahamba25@gmail.com>
2022-11-12 15:08:10 +0100

    adicionei novas classes no componente Input

--- src/Components/Forms/Input.jsx
+++ src/Components/Forms/Input.jsx
@@ -1,18 +1,14 @@
 import React from "react";
 import styles from "./Input.module.css";
 
-const Input = ({ label, type, name, value }) => {
+const Input = ({ label, type, name }) => {
   return (
-    <div>
-      <label htmlFor={name}>{label}</label>
-      <input
-        id={name}
-        name={name}
-        type={type}
-        value={value}
-        className={styles.input}
-      />
-      {value}
+    <div className={styles.wrapper}>
+      <label className={styles.label} htmlFor={name}>
+        {label}
+      </label>
+      <input id={name} name={name} type={type} className={styles.input} />
+      <p className={styles.error}>Error</p>
     </div>
   );
 };
