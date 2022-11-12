a614426d1ff7c58bcd8702c78b9a4fea0d68668f
pascoalkahamba <pascoalkahamba25@gmail.com>
2022-11-12 13:23:18 +0100

    criei o formulario e os estados username e userpassword no loginForm

--- src/Components/Login/LoginForm.jsx
+++ src/Components/Login/LoginForm.jsx
@@ -1,10 +1,24 @@
-import React from "react";
+import React, { useState } from "react";
 import { Link } from "react-router-dom";
 
 const LoginForm = () => {
+  const [username, setUsername] = useState("");
+  const [userpassword, setUserpassword] = useState("");
   return (
     <section>
       <h1>Login</h1>
+      <form action="">
+        <input
+          type="text"
+          value={username}
+          onChange={({ target }) => setUsername(target.value)}
+        />
+        <input
+          type="password"
+          value={userpassword}
+          onChange={({ target }) => setUserpassword(target.value)}
+        />
+      </form>
       <Link to="/login/criar">Cadastro</Link>
     </section>
   );
