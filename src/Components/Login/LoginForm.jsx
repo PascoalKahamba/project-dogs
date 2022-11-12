4fdb2831ba02a26e3082aa697f4691b64d908173
pascoalkahamba <pascoalkahamba25@gmail.com>
2022-11-12 13:44:02 +0100

    criei a função handleSubmit com um fetch no componente loginForm

--- src/Components/Login/LoginForm.jsx
+++ src/Components/Login/LoginForm.jsx
@@ -4,10 +4,27 @@ import { Link } from "react-router-dom";
 const LoginForm = () => {
   const [username, setUsername] = useState("");
   const [userpassword, setUserpassword] = useState("");
+  function handleSubmit(event) {
+    event.preventDefault();
+    fetch("https://dogsapi.origamid.dev/json/jwt-auth/v1/token", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({ username, userpassword }),
+    })
+      .then((response) => {
+        console.log(response);
+        return response.json();
+      })
+      .then((json) => {
+        console.log(json);
+      });
+  }
   return (
     <section>
       <h1>Login</h1>
-      <form action="">
+      <form action="" onSubmit={handleSubmit}>
         <input
           type="text"
           value={username}
@@ -18,6 +35,7 @@ const LoginForm = () => {
           value={userpassword}
           onChange={({ target }) => setUserpassword(target.value)}
         />
+        <button>Entrar</button>
       </form>
       <Link to="/login/criar">Cadastro</Link>
     </section>
