53f61bc74e927b501efc1442259ae8ad30a5562f
pascoalkahamba <pascoalkahamba25@gmail.com>
2022-11-14 23:17:45 +0100

    tirei o UserContext em LoginForm e adicionei varios hooks em UserContext

--- src/UserContext.jsx
+++ src/UserContext.jsx
@@ -1,7 +1,11 @@
-import React, { createContext } from "react";
+import React, { createContext, useState } from "react";
 export const UserContext = createContext();
 
 export const UserStorage = ({ children }) => {
+  const [data, setData] = useState(null);
+  const [login, setLogin] = useState(null);
+  const [loading, setLoading] = useState(false);
+  const [error, setError] = useState(null);
   return (
     <UserContext.Provider value={{ usuario: "Pascoal" }}>
       {" "}
