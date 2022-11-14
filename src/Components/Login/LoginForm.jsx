7ad6c6e33daedfc7c8451e4e77656706b312dbd7
pascoalkahamba <pascoalkahamba25@gmail.com>
2022-11-14 21:28:16 +0100

    criei um useEffect para puxar o token no localStore

--- src/Components/Login/LoginForm.jsx
+++ src/Components/Login/LoginForm.jsx
@@ -1,18 +1,24 @@
-import React from "react";
+import React, { useEffect } from "react";
 import { Link } from "react-router-dom";
 import { TOKEN_POST, USER_GET } from "../../api";
 import useForm from "../../Hooks/useForm";
 import Button from "../Forms/Button";
 import Input from "../Forms/Input";
-async function getUser(token) {
-  const { url, options } = USER_GET(token);
-  const response = await fetch(url, options);
-  const json = await response.json();
-  console.log(json);
-}
+
 const LoginForm = () => {
   const username = useForm();
   const password = useForm();
+
+  useEffect(() => {
+    const token = window.localStorage.getItem("token");
+    if (token) getUser(token);
+  }, []);
+  async function getUser(token) {
+    const { url, options } = USER_GET(token);
+    const response = await fetch(url, options);
+    const json = await response.json();
+    console.log(json);
+  }
   async function handleSubmit(event) {
     event.preventDefault();
     if (username.validate() && password.validate()) {
