6766e798e3d3cefe2694c75db15133fb66643669
pascoalkahamba <pascoalkahamba25@gmail.com>
2022-11-14 04:49:56 +0100

    criei a função validate

--- src/Hooks/useForm.jsx
+++ src/Hooks/useForm.jsx
@@ -7,9 +7,16 @@ const types = {
   },
 };
 
-const useForm = () => {
+const useForm = (type) => {
   const [value, setValue] = useState("");
-
+  const [error, setError] = useState(null);
+  function validate(value) {
+    if (type === false) return true;
+    if (value.length === 0) {
+      setError("Preencha um valor");
+      return false;
+    }
+  }
   function onChange({ target }) {
     setValue(target.value);
   }
