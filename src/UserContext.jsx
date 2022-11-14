34c144b489d90936719342074e0ea5c55ccc39a9
pascoalkahamba <pascoalkahamba25@gmail.com>
2022-11-14 23:27:30 +0100

    criei a função userLogin em UserContext

--- src/UserContext.jsx
+++ src/UserContext.jsx
@@ -1,4 +1,5 @@
 import React, { createContext, useState } from "react";
+import { TOKEN_POST } from "./api";
 export const UserContext = createContext();
 
 export const UserStorage = ({ children }) => {
@@ -6,6 +7,12 @@ export const UserStorage = ({ children }) => {
   const [login, setLogin] = useState(null);
   const [loading, setLoading] = useState(false);
   const [error, setError] = useState(null);
+  async function userLogin(username, password) {
+    const { url, options } = TOKEN_POST({ username, password });
+    const tokenRes = await fetch(url, options);
+    const { token } = await tokenRes.json();
+    window.localStorage.setItem("token", token);
+  }
   return (
     <UserContext.Provider value={{ usuario: "Pascoal" }}>
       {" "}
