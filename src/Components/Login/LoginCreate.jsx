9fb06aabad57389030aef900c6364bfda4e03892
pascoalkahamba <pascoalkahamba25@gmail.com>
2022-11-28 23:33:42 +0100

    importei o USER_POST em LoginCreate

--- src/Components/Login/LoginCreate.jsx
+++ src/Components/Login/LoginCreate.jsx
@@ -2,13 +2,21 @@ import React from "react";
 import Input from "../Forms/Input";
 import Button from "../Forms/Button";
 import useForm from "../../Hooks/useForm";
+import { USER_POST } from "../../api";
 
 const LoginCreate = () => {
   const username = useForm();
   const email = useForm("email");
   const password = useForm();
-  function handleSubmit(event) {
+  async function handleSubmit(event) {
     event.preventDefault();
+    const { url, options } = USER_POST({
+      username: username.value,
+      email: email.value,
+      password: password.value,
+    });
+    const response = await fetch(url, options);
+    console.log(response);
   }
   return (
     <section className="animeLeft">
