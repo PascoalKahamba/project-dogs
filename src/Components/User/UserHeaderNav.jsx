de29facdd5c0de1910422ab2992c07944944d311
pascoalkahamba <pascoalkahamba25@gmail.com>
2022-11-29 19:03:25 +0100

    utilizei o userLogout em UserHeaderNav

--- src/Components/User/UserHeaderNav.jsx
+++ src/Components/User/UserHeaderNav.jsx
@@ -1,13 +1,15 @@
-import React from "react";
+import React, { useContext } from "react";
 import { NavLink } from "react-router-dom";
+import { UserContext } from "../../UserContext";
 
 const UserHeaderNav = () => {
+  const { userLogout } = useContext(UserContext);
   return (
     <nav>
       <NavLink to="/conta">Minahs fotos</NavLink>
       <NavLink to="/conta/estatisticas">Estatisticas</NavLink>
       <NavLink to="/conta/postar">Adicionar fotos</NavLink>
-      <button>Sair</button>
+      <button onClick={userLogout}>Sair</button>
     </nav>
   );
 };
