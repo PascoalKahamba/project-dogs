44878641888b568babdbc757567e409fc90fd25d
pascoalkahamba <pascoalkahamba25@gmail.com>
2022-11-29 18:53:37 +0100

    adicionei um NavLink em UserHeaderNav

--- src/Components/User/UserHeaderNav.jsx
+++ src/Components/User/UserHeaderNav.jsx
@@ -1,7 +1,15 @@
 import React from "react";
+import { NavLink } from "react-router-dom";
 
 const UserHeaderNav = () => {
-  return <div>Navegação</div>;
+  return (
+    <nav>
+      <NavLink to="/conta">Minahs fotos</NavLink>
+      <NavLink to="/conta/estatisticas">Estatisticas</NavLink>
+      <NavLink to="/conta/postar">Adicionar fotos</NavLink>
+      <button>Sair</button>
+    </nav>
+  );
 };
 
 export default UserHeaderNav;
