a77e41d5ec258cb13db7412ff5ecea75b3e348ad
pascoalkahamba <pascoalkahamba25@gmail.com>
2022-11-29 05:33:33 +0100

    mudei o ProtectedRoute pelo Route em App

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,7 @@ import { BrowserRouter, Route, Routes } from "react-router-dom";
 import "./App.css";
 import Footer from "./Components/Footer";
 import Header from "./Components/Header";
-import ProtectedRoute from "./Components/Helper/ProtectedRoute";
+// import ProtectedRoute from "./Components/Helper/ProtectedRoute";
 import Home from "./Components/Home";
 import Login from "./Components/Login/Login";
 import User from "./Components/User/User";
@@ -18,7 +18,7 @@ const App = () => {
           <Routes>
             <Route path="/" element={<Home />} />
             <Route path="login/*" element={<Login />} />
-            <ProtectedRoute path="conta/*" element={<User />} />
+            <Route path="conta/*" element={<User />} />
           </Routes>
           <Footer />
         </UserStorage>
