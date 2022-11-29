b4f753fe7ba261dce0c7fa1c655dfe7cde3bd54d
pascoalkahamba <pascoalkahamba25@gmail.com>
2022-11-29 04:35:51 +0100

    criei o componente ProtectedRoute

---
+++ src/Components/Helper/ProtectedRoute.jsx
@@ -0,0 +1,12 @@
+import React, { useContext } from "react";
+import { Navigate, Route } from "react-router-dom";
+import { UserContext } from "../../UserContext";
+
+const ProtectedRoute = (props) => {
+  const { login } = useContext(UserContext);
+  if (login === true) return <Route {...props} />;
+  else if (login === false) return <Navigate to="/login" />;
+  return <div>ProtectedRoute</div>;
+};
+
+export default ProtectedRoute;
