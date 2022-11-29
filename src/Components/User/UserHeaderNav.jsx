c461969b5b3a7713af2ed2d7017197fab489d5cb
pascoalkahamba <pascoalkahamba25@gmail.com>
2022-11-29 20:04:48 +0100

    importei as fotos em UserHeaderNav

--- src/Components/User/UserHeaderNav.jsx
+++ src/Components/User/UserHeaderNav.jsx
@@ -1,15 +1,31 @@
 import React, { useContext } from "react";
 import { NavLink } from "react-router-dom";
 import { UserContext } from "../../UserContext";
+import minhasFotos from "../../assets/feed.svg";
+import estatisticas from "../../assets/estatisticas.svg";
+import adicionarFotos from "../../assets/adicionar.svg";
+import sair from "../../assets/sair.svg";
 
 const UserHeaderNav = () => {
   const { userLogout } = useContext(UserContext);
   return (
     <nav>
-      <NavLink to="/conta">Minahs fotos</NavLink>
-      <NavLink to="/conta/estatisticas">Estatisticas</NavLink>
-      <NavLink to="/conta/postar">Adicionar fotos</NavLink>
-      <button onClick={userLogout}>Sair</button>
+      <NavLink to="/conta">
+        <img src={minhasFotos} />
+        Minahs fotos
+      </NavLink>
+      <NavLink to="/conta/estatisticas">
+        <img src={estatisticas} />
+        Estatisticas
+      </NavLink>
+      <NavLink to="/conta/postar">
+        <img src={adicionarFotos} />
+        Adicionar fotos
+      </NavLink>
+      <button onClick={userLogout}>
+        <img src={sair} />
+        Sair
+      </button>
     </nav>
   );
 };
