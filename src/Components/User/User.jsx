0cec7c4effd4b0ebc7a7ad4764b8742af577493c
pascoalkahamba <pascoalkahamba25@gmail.com>
2022-11-29 06:59:38 +0100

    criei o componente Feed

--- src/Components/User/User.jsx
+++ src/Components/User/User.jsx
@@ -1,10 +1,15 @@
 import React from "react";
+import { Route, Routes } from "react-router-dom";
+import Feed from "../Feed/Feed";
 import UserHeader from "./UserHeader";
 
 const User = () => {
   return (
     <section className="container">
       <UserHeader />
+      <Routes>
+        <Route path="/" element={<Feed />} />
+      </Routes>
     </section>
   );
 };
