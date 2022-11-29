82e99bd0b478650e8a2bf471f3c9f262731b5a8f
pascoalkahamba <pascoalkahamba25@gmail.com>
2022-11-29 22:20:30 +0100

    criei um switch para saber em que página o usuário esta em UserHeader

--- src/Components/User/UserHeader.jsx
+++ src/Components/User/UserHeader.jsx
@@ -1,11 +1,29 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import UserHeaderNav from "./UserHeaderNav";
 import styles from "./UserHeader.module.css";
+import { useLocation } from "react-router-dom";
 
 const UserHeader = () => {
+  const [title, setTitle] = useState("");
+  const location = useLocation();
+  console.log(location);
+  useEffect(() => {
+    const { pathname } = location;
+    switch (pathname) {
+      case "/conta/postar":
+        setTitle("Poste sua Foto");
+        break;
+      case "/conta/estatisticas":
+        setTitle("Estatísticas");
+        break;
+      default:
+        setTitle("Minha Conta");
+    }
+  }, [location]);
+
   return (
     <header className={styles.header}>
-      <h1 className="title">Titulo</h1>
+      <h1 className="title">{title}</h1>
       <UserHeaderNav />
     </header>
   );
