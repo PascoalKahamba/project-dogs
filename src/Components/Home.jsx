117cb3391c9f41e463345bd5b281765418eb8d19
pascoalkahamba <pascoalkahamba25@gmail.com>
2022-11-29 20:47:08 +0100

    adicionei os primeiros estilos nos a e button

--- src/Components/Home.jsx
+++ src/Components/Home.jsx
@@ -1,7 +1,13 @@
 import React from "react";
+import UserHeaderNav from "./User/UserHeaderNav";
 
 const Home = () => {
-  return <div>Home</div>;
+  return (
+    <div>
+      Home
+      <UserHeaderNav />
+    </div>
+  );
 };
 
 export default Home;
