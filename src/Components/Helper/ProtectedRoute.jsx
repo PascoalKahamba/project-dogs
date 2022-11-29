803bea3d34a696309ba7ee9d5cb0e950a6279e18
pascoalkahamba <pascoalkahamba25@gmail.com>
2022-11-29 05:07:36 +0100

    criei um senão quando não for true nem null o login em ProtectedRoute e um senão no UserContext para setar o setLogin como False

--- src/Components/Helper/ProtectedRoute.jsx
+++ src/Components/Helper/ProtectedRoute.jsx
@@ -4,9 +4,10 @@ import { UserContext } from "../../UserContext";
 
 const ProtectedRoute = (props) => {
   const { login } = useContext(UserContext);
+
   if (login === true) return <Route {...props} />;
   else if (login === false) return <Navigate to="/login" />;
-  return <div>ProtectedRoute</div>;
+  else return null;
 };
 
 export default ProtectedRoute;
