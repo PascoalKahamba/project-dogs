a0088ab868aa91fe7b34e0bc0884e03a2e10ed78
pascoalkahamba <pascoalkahamba25@gmail.com>
2022-12-01 08:33:22 +0100

    importei os inputs e button em UserPhotoPost

--- src/Components/User/UserPhotoPost.jsx
+++ src/Components/User/UserPhotoPost.jsx
@@ -1,13 +1,22 @@
 import React from "react";
 import styles from "./UserPhotoPost.module.css";
+import Input from "../Forms/Input";
+import Button from "../Forms/Button";
 
 const UserPhotoPost = () => {
   function handleSubmit(event) {
     event.preventDefault();
   }
+  function handleImgChange() {}
   return (
     <section className={`${styles.photoPost} animeLeft`}>
-      <form onSubmit={handleSubmit}></form>
+      <form onSubmit={handleSubmit}>
+        <Input label="Nome" type="text" name="nome" />
+        <Input label="Peso" type="text" name="peso" />
+        <Input label="Idade" type="text" name="idade" />
+        <input type="file" name="img" id="img" onChange={handleImgChange} />
+        <Button>Enviar</Button>
+      </form>
     </section>
   );
 };
