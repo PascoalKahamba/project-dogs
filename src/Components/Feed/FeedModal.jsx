143c2ca6f390992c81f4498e6631e04e719e0459
pascoalkahamba <pascoalkahamba25@gmail.com>
2022-12-02 16:50:07 +0100

    criei um useEffect e a função fetchPhotos no FeedModal

--- src/Components/Feed/FeedModal.jsx
+++ src/Components/Feed/FeedModal.jsx
@@ -1,8 +1,15 @@
-import React from "react";
+import React, { useEffect } from "react";
 import useFetch from "../../Hooks/useFetch";
 
 const FeedModal = () => {
   const { data, loading, error, request } = useFetch();
+  useEffect(() => {
+    async function fetchPhotos() {
+      const { url, options } = PHOTOS_GET();
+      request(url, options);
+    }
+    fetchPhotos();
+  }, []);
   return <div>FeedModal</div>;
 };
 
