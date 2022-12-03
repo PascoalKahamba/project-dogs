056a3e29409464bb35538bcaabd624f740917bc6
pascoalkahamba <pascoalkahamba25@gmail.com>
2022-12-03 06:04:18 +0100

    criei os componentes de estilos do FeedPhotos e FeedPhotosItem

--- src/Components/Feed/FeedPhotos.jsx
+++ src/Components/Feed/FeedPhotos.jsx
@@ -4,13 +4,14 @@ import useFetch from "../../Hooks/useFetch";
 import { PHOTOS_GET } from "../../api";
 import Error from "../Helper/Error";
 import Loading from "./Loading";
+import styles from "./FeedPhotos.module.css";
 
 const FeedPhotos = () => {
   const { data, loading, error, request } = useFetch();
   useEffect(() => {
     async function fetchPhotos() {
       const { url, options } = PHOTOS_GET({ page: 1, total: 6, user: 0 });
-      const { response, json } = request(url, options);
+      const { json } = request(url, options);
       console.log(json);
     }
     fetchPhotos();
@@ -19,7 +20,7 @@ const FeedPhotos = () => {
   if (loading) return <Loading />;
   if (data)
     return (
-      <ul>
+      <ul className={`${styles.feed} animeLeft`}>
         {data.map((photo) => (
           <FeedPhotosItem key={photo.id} photo={photo} />
         ))}
