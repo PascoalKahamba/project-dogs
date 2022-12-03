75eb8edc26c9597334c0d337d63d37e386eca858
pascoalkahamba <pascoalkahamba25@gmail.com>
2022-12-03 08:43:59 +0100

    importei o estilo de PhotoContent e usei o data em PhotoContent

--- src/Components/Photo/PhotoContent.jsx
+++ src/Components/Photo/PhotoContent.jsx
@@ -1,8 +1,14 @@
 import React from "react";
-import styles from "./PhotoContent.mdoule.css";
+import styles from "./PhotoContent.module.css";
 
 const PhotoContent = ({ data }) => {
-  return <div></div>;
+  const { photo, comnents } = data;
+  console.log(data);
+  return (
+    <div className={styles.photo}>
+      <div className={styles.img}></div>
+    </div>
+  );
 };
 
 export default PhotoContent;
