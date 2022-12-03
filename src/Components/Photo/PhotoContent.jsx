691935d842f238b0d75ff4f1bf987fc898e2e4df
pascoalkahamba <pascoalkahamba25@gmail.com>
2022-12-03 09:13:59 +0100

    criei um link para a conta do autor que postou a foto

--- src/Components/Photo/PhotoContent.jsx
+++ src/Components/Photo/PhotoContent.jsx
@@ -1,12 +1,20 @@
 import React from "react";
+import { Link } from "react-router-dom";
 import styles from "./PhotoContent.module.css";
 
 const PhotoContent = ({ data }) => {
   const { photo, comnents } = data;
-  console.log(data);
+
   return (
     <div className={styles.photo}>
-      <div className={styles.img}></div>
+      <div className={styles.img}>
+        <img src={photo.src} alt={photo.title} />
+      </div>
+      <div className={styles.details}>
+        <p>
+          <Link to={`/perfil/${photo.author}`}>@{photo.author}</Link>
+        </p>
+      </div>
     </div>
   );
 };
