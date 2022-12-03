4ab69ce9fc7f4f8de309a3cb4f6b6fc6245e4a72
pascoalkahamba <pascoalkahamba25@gmail.com>
2022-12-03 07:45:22 +0100

    criei um componente para estilos do FeedModal

--- src/Components/Feed/FeedModal.jsx
+++ src/Components/Feed/FeedModal.jsx
@@ -1,7 +1,10 @@
 import React from "react";
+import styles from "./FeedMOdal.module.css";
+import useFetch from "../../Hooks/useFetch";
 
 const FeedModal = () => {
-  return <div>FeedModal</div>;
+  const { data, loading, error, request } = useFetch();
+  return <div className={styles.modal}>FeedModal</div>;
 };
 
 export default FeedModal;
