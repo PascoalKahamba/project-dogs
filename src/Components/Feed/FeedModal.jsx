e3f475a1ff282251f3b1d81babe55680c8b69464
pascoalkahamba <pascoalkahamba25@gmail.com>
2022-12-03 08:23:50 +0100

    utilizei o data error e loading no FeedModal

--- src/Components/Feed/FeedModal.jsx
+++ src/Components/Feed/FeedModal.jsx
@@ -1,7 +1,9 @@
 import React, { useEffect } from "react";
 import styles from "./FeedMOdal.module.css";
 import useFetch from "../../Hooks/useFetch";
+import Error from "../Helper/Error";
 import { PHOTO_GET } from "../../api";
+import Loading from "./Loading";
 
 const FeedModal = ({ photo }) => {
   const { data, loading, error, request } = useFetch();
@@ -9,7 +11,14 @@ const FeedModal = ({ photo }) => {
     const { url, options } = PHOTO_GET(photo.id);
     request(url, options);
   }, [photo, request]);
-  return <div className={styles.modal}>FeedModal</div>;
+  return (
+    <div className={styles.modal}>
+      {error && <Error error={error} />}
+      {loading && <Loading />}
+      {data && <PhotoContent />}
+      <img src={photo.src} alt="" />
+    </div>
+  );
 };
 
 export default FeedModal;
