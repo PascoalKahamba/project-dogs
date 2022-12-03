4338debd39cb68c11a35f9c5852315b976be1266
pascoalkahamba <pascoalkahamba25@gmail.com>
2022-12-03 02:38:26 +0100

    melhorando o codigo tirei o codigo no FeedModal para FeedPhotos

--- src/Components/Feed/FeedModal.jsx
+++ src/Components/Feed/FeedModal.jsx
@@ -1,15 +1,6 @@
-import React, { useEffect } from "react";
-import useFetch from "../../Hooks/useFetch";
+import React from "react";
 
 const FeedModal = () => {
-  const { data, loading, error, request } = useFetch();
-  useEffect(() => {
-    async function fetchPhotos() {
-      const { url, options } = PHOTOS_GET({ page: 1, total: 6, user: 0 });
-      request(url, options);
-    }
-    fetchPhotos();
-  }, []);
   return <div>FeedModal</div>;
 };
 
--- src/Components/Feed/FeedPhotos.jsx
+++ src/Components/Feed/FeedPhotos.jsx
@@ -1,7 +1,16 @@
 import React from "react";
 import FeedPhotosItem from "./FeedPhotosItem";
+import useFetch from "../../Hooks/useFetch";
 
 const FeedPhotos = () => {
+  const { data, loading, error, request } = useFetch();
+  useEffect(() => {
+    async function fetchPhotos() {
+      const { url, options } = PHOTOS_GET({ page: 1, total: 6, user: 0 });
+      request(url, options);
+    }
+    fetchPhotos();
+  }, []);
   return (
     <div>
       <FeedPhotosItem />
