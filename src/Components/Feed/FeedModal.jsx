c5f8758c8f769d36e354ab6ca8a6740a41b22815
pascoalkahamba <pascoalkahamba25@gmail.com>
2022-12-03 07:50:00 +0100

    utilizei um useEffect no FeedModal

--- src/Components/Feed/FeedModal.jsx
+++ src/Components/Feed/FeedModal.jsx
@@ -1,9 +1,13 @@
-import React from "react";
+import React, { useEffect } from "react";
 import styles from "./FeedMOdal.module.css";
 import useFetch from "../../Hooks/useFetch";
 
-const FeedModal = () => {
+const FeedModal = ({ photo }) => {
   const { data, loading, error, request } = useFetch();
+  useEffect(() => {
+    const { url, options } = PHOTO_GET();
+    request(url, options);
+  }, [photo, request]);
   return <div className={styles.modal}>FeedModal</div>;
 };
 
