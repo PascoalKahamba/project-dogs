072fa6ffd6ecd56c8a677b3a2665131574caf026
pascoalkahamba <pascoalkahamba25@gmail.com>
2022-12-03 07:55:41 +0100

    criei a função PHOTO_GET na api e

--- src/Components/Feed/FeedModal.jsx
+++ src/Components/Feed/FeedModal.jsx
@@ -1,11 +1,12 @@
 import React, { useEffect } from "react";
 import styles from "./FeedMOdal.module.css";
 import useFetch from "../../Hooks/useFetch";
+import { PHOTO_GET } from "../../api";
 
 const FeedModal = ({ photo }) => {
   const { data, loading, error, request } = useFetch();
   useEffect(() => {
-    const { url, options } = PHOTO_GET();
+    const { url, options } = PHOTO_GET(photo.id);
     request(url, options);
   }, [photo, request]);
   return <div className={styles.modal}>FeedModal</div>;
