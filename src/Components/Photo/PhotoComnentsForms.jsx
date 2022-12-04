e0e25c20cf743fef1104f0395f522760a3439c99
pascoalkahamba <pascoalkahamba25@gmail.com>
2022-12-04 07:02:14 +0100

    adicionei um id name e placeholder no textarea

--- src/Components/Photo/PhotoComnentsForms.jsx
+++ src/Components/Photo/PhotoComnentsForms.jsx
@@ -1,13 +1,20 @@
 import React, { useState } from "react";
+import enviar from "../../assets/enviar.svg";
 
 const PhotoComnentsForms = ({ id }) => {
   const { comnents, setComnents } = useState("");
   return (
     <form>
       <textarea
+        id="comnents"
+        name="comnents"
+        placeholder="Comente..."
         value={comnents}
         onChange={({ target }) => setComnents(target.value)}
       />
+      <button>
+        <img src={enviar} alt="image icon" />
+      </button>
     </form>
   );
 };
