9e34ae1c9224e89e6a8fa51a646bf396580d15f2
pascoalkahamba <pascoalkahamba25@gmail.com>
2022-12-04 08:16:42 +0100

    criei o componente de estilos para PhotoComments e alguns elementos

--- src/Components/Photo/PhotoComnents.jsx
+++ src/Components/Photo/PhotoComnents.jsx
@@ -1,10 +1,25 @@
-import React, { useContext } from "react";
+import React, { useContext, useState } from "react";
 import { UserContext } from "../../UserContext";
 import PhotoComnentsForms from "./PhotoComnentsForms";
+import styles from "./PhotoComments.module.css";
 
 const PhotoComnents = (props) => {
+  const [comments, setComments] = useState(() => props.comments);
   const { login } = useContext(UserContext);
-  return <div>{login && <PhotoComnentsForms id={props.id} />}</div>;
+  return (
+    <div>
+      <ul className={styles.comments}>
+        {comments.map((comment) => (
+          <li key={comment.comment_ID}>
+            <b>{comment.comment_author}: </b>
+            <span>{comment.comment_content}</span>
+          </li>
+        ))}
+      </ul>
+
+      {login && <PhotoComnentsForms id={props.id} />}
+    </div>
+  );
 };
 
 export default PhotoComnents;
