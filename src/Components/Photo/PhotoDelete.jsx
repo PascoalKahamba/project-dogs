5ea8f43ae29706ccc3148df12ef45ea6c5dbcb9c
pascoalkahamba <pascoalkahamba25@gmail.com>
2022-12-05 09:41:38 +0100

    utilizei o hook useFetch no PhotoDelete

--- src/Components/Photo/PhotoDelete.jsx
+++ src/Components/Photo/PhotoDelete.jsx
@@ -5,10 +5,13 @@ import styles from "./PhotoDelete.module.css";
 
 const PhotoDelete = ({ id }) => {
   const { loading, request } = useFetch();
+
   async function handleClick(event) {
     event.preventDefault();
     const { url, options } = PHOTO_DELETE(id);
     const { response } = await request(url, options);
+    if (response.ok) {
+    }
   }
   return (
     <div>
