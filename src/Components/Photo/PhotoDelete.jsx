b2b1bb505ed71db27091967187d60f3ab31162ca
pascoalkahamba <pascoalkahamba25@gmail.com>
2022-12-05 09:29:13 +0100

    criei a função handleClick no PhotoDelete

--- src/Components/Photo/PhotoDelete.jsx
+++ src/Components/Photo/PhotoDelete.jsx
@@ -1,10 +1,17 @@
 import React from "react";
+import { PHOTO_DELETE } from "../../api";
 import styles from "./PhotoDelete.module.css";
 
 const PhotoDelete = ({ id }) => {
+  async function handleClick(event) {
+    event.preventDefault();
+    const { url, options } = PHOTO_DELETE(id);
+  }
   return (
     <div>
-      <button className={styles.delete}>Deletar</button>
+      <button onClick={handleClick} className={styles.delete}>
+        Deletar
+      </button>
     </div>
   );
 };
