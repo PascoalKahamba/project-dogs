241821bd6ca79970af60a147cc81d8f656663de7
pascoalkahamba <pascoalkahamba25@gmail.com>
2022-12-06 14:09:47 +0100

    criei a função infiniteScroll em Feed

--- src/Components/Feed/Feed.jsx
+++ src/Components/Feed/Feed.jsx
@@ -1,9 +1,20 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import FeedModal from "./FeedModal";
 import FeedPhotos from "./FeedPhotos";
 
 const Feed = ({ user }) => {
   const [modalPhoto, setModalPhoto] = useState(null);
+
+  useEffect(() => {
+    function infiniteScroll() {}
+    window.addEventListener("wheel", infiniteScroll);
+    window.addEventListener("scroll", infiniteScroll);
+    return () => {
+      window.removeEventListener("wheel", infiniteScroll);
+      window.removeEventListener("scroll", infiniteScroll);
+    };
+  }, []);
+
   return (
     <section>
       {modalPhoto && (
