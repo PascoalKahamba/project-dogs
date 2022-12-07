0d61e42e01e5128a85486a0399ef732a78a07f13
pascoalkahamba <pascoalkahamba25@gmail.com>
2022-12-07 10:57:43 +0100

    utilizei a url e options para resetar a password

--- src/Components/Login/LoginPasswordLost.jsx
+++ src/Components/Login/LoginPasswordLost.jsx
@@ -3,19 +3,30 @@ import Input from "../Forms/Input";
 import Button from "../Forms/Button";
 import useForm from "../../Hooks/useForm";
 import useFetch from "../../Hooks/useFetch";
+import { PASSWORD_LOST } from "../../api";
 
 const LoginPasswordLost = () => {
-  const email = useForm();
+  const login = useForm();
   const { data, error, loading, request } = useFetch();
   async function handleSubmit(event) {
     event.preventDefault();
+    const { url, options } = PASSWORD_LOST({
+      login: login.value,
+      url: window.location.href.replace("perdeu", "resetar"),
+    });
+    request(url, options);
   }
   return (
     <section>
       <h1 className="title">Perdeu a senha?</h1>
       <form onSubmit={handleSubmit}>
-        <Input label="Email / Usuário" type="text" name="email" {...email} />
-        <Button>Enviar Email</Button>
+        <Input label="Email / Usuário" type="text" name="login" {...login} />
+
+        {loading ? (
+          <Button disabled>Enviando...</Button>
+        ) : (
+          <Button>Enviar Email</Button>
+        )}
       </form>
     </section>
   );
