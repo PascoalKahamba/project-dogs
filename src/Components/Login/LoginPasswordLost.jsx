07e756035534c977a14dfeb0c4f495c7d20c2dae
pascoalkahamba <pascoalkahamba25@gmail.com>
2022-12-07 11:22:50 +0100

    criei uma condição quando o data existir

--- src/Components/Login/LoginPasswordLost.jsx
+++ src/Components/Login/LoginPasswordLost.jsx
@@ -3,6 +3,7 @@ import Input from "../Forms/Input";
 import Button from "../Forms/Button";
 import useForm from "../../Hooks/useForm";
 import useFetch from "../../Hooks/useFetch";
+import Error from "../Helper/Error";
 import { PASSWORD_LOST } from "../../api";
 
 const LoginPasswordLost = () => {
@@ -10,24 +11,33 @@ const LoginPasswordLost = () => {
   const { data, error, loading, request } = useFetch();
   async function handleSubmit(event) {
     event.preventDefault();
-    const { url, options } = PASSWORD_LOST({
-      login: login.value,
-      url: window.location.href.replace("perdeu", "resetar"),
-    });
-    request(url, options);
+    if (login.validate()) {
+      const { url, options } = PASSWORD_LOST({
+        login: login.value,
+        url: window.location.href.replace("perdeu", "resetar"),
+      });
+      const { json } = await request(url, options);
+      console.log(json);
+    }
   }
   return (
     <section>
       <h1 className="title">Perdeu a senha?</h1>
-      <form onSubmit={handleSubmit}>
-        <Input label="Email / Usuário" type="text" name="login" {...login} />
+      {data ? (
+        <p style={{ color: "#4c1" }}>{data}</p>
+      ) : (
+        <form onSubmit={handleSubmit}>
+          <Input label="Email / Usuário" type="text" name="login" {...login} />
 
-        {loading ? (
-          <Button disabled>Enviando...</Button>
-        ) : (
-          <Button>Enviar Email</Button>
-        )}
-      </form>
+          {loading ? (
+            <Button disabled>Enviando...</Button>
+          ) : (
+            <Button>Enviar Email</Button>
+          )}
+        </form>
+      )}
+
+      {error && <Error error={error} />}
     </section>
   );
 };
