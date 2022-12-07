c9ee0f31c1ead76b7088898df39d9a4ffca8143c
pascoalkahamba <pascoalkahamba25@gmail.com>
2022-12-07 10:23:03 +0100

    primeiros codigos no componente LoginPasswordLost

--- src/Components/Login/LoginPasswordLost.jsx
+++ src/Components/Login/LoginPasswordLost.jsx
@@ -1,7 +1,20 @@
 import React from "react";
+import Input from "../Forms/Input";
+import Button from "../Forms/Button";
 
 const LoginPasswordLost = () => {
-  return <div>LoginPasswordLost</div>;
+  async function handleSubmit(event) {
+    event.preventDefault();
+  }
+  return (
+    <section>
+      <h1 className="title">Perdeu a senha?</h1>
+      <form onSubmit={handleSubmit}>
+        <Input />
+        <Button>Enviar Email</Button>
+      </form>
+    </section>
+  );
 };
 
 export default LoginPasswordLost;
