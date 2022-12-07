8ac56113faf21c9fa0a278a4d509059dc6f71882
pascoalkahamba <pascoalkahamba25@gmail.com>
2022-12-07 10:48:38 +0100

    criei a função PASSWORD_LOST

--- src/Components/Login/LoginPasswordLost.jsx
+++ src/Components/Login/LoginPasswordLost.jsx
@@ -2,9 +2,11 @@ import React from "react";
 import Input from "../Forms/Input";
 import Button from "../Forms/Button";
 import useForm from "../../Hooks/useForm";
+import useFetch from "../../Hooks/useFetch";
 
 const LoginPasswordLost = () => {
   const email = useForm();
+  const { data, error, loading, request } = useFetch();
   async function handleSubmit(event) {
     event.preventDefault();
   }
