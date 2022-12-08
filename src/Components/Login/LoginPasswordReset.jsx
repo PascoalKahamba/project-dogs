2d81db8ee6504646bddcbc596801fcb8ada7fcca
pascoalkahamba <pascoalkahamba25@gmail.com>
2022-12-08 02:49:19 +0100

    utilizando o loading e o error

--- src/Components/Login/LoginPasswordReset.jsx
+++ src/Components/Login/LoginPasswordReset.jsx
@@ -3,21 +3,27 @@ import Input from "../Forms/Input";
 import Button from "../Forms/Button";
 import useForm from "../../Hooks/useForm";
 import useFetch from "../../Hooks/useFetch";
+import Error from "../Helper/Error";
 import { PASSWORD_RESET } from "../../api";
+import { useNavigate } from "react-router-dom";
 
 const LoginPasswordReset = () => {
   const [login, setLogin] = useState("");
   const [key, setKey] = useState("");
   const password = useForm();
+  const navigate = useNavigate();
   const { error, loading, request } = useFetch();
   async function handleSubmit(event) {
-    event.preventDefaul();
-    const { url, options } = PASSWORD_RESET({
-      login,
-      key,
-      password: password.value,
-    });
-    await request(url, options);
+    event.preventDefault();
+    if (password.validate()) {
+      const { url, options } = PASSWORD_RESET({
+        login,
+        key,
+        password: password.value,
+      });
+      const { response } = await request(url, options);
+      if (response.ok) navigate("/login");
+    }
   }
 
   useEffect(() => {
@@ -29,8 +35,7 @@ const LoginPasswordReset = () => {
   }, []);
   return (
     <div>
-      {key}
-      {login}
+      <h1 className="title">Resete a Senha</h1>
       <form onSubmit={handleSubmit}>
         <Input
           label="Nova Senha"
@@ -38,8 +43,13 @@ const LoginPasswordReset = () => {
           name="password"
           {...password}
         />
-        <Button>Resetar</Button>
+        {loading ? (
+          <Button disabled>Resetando...</Button>
+        ) : (
+          <Button>Resetar</Button>
+        )}
       </form>
+      {error && <Error error={error} />}
     </div>
   );
 };
