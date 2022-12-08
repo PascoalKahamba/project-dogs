d5f29ff4a6a2164ba10655af6fd765fdc0871343
pascoalkahamba <pascoalkahamba25@gmail.com>
2022-12-08 09:52:42 +0100

    importei os graficos do victory e criei alguns condições

--- src/Components/User/UserStatsGraphs.jsx
+++ src/Components/User/UserStatsGraphs.jsx
@@ -1,16 +1,27 @@
 import React, { useEffect, useState } from "react";
 import styles from "./UserStatsGraphs.module.css";
+import { VictoryPie, VictoryChart, VictoryBar } from "victory";
 
 const UserStatsGraphs = ({ data }) => {
   const [graph, setGraph] = useState([]);
   const [total, setTotal] = useState(0);
   useEffect(() => {
+    const graphData = data.map((item) => {
+      return {
+        x: item.title,
+        y: Number(item.acessos),
+      };
+    });
     setTotal(data.map(({ acessos }) => +acessos).reduce((a, b) => a + b));
-  });
+    setGraph(graphData);
+  }, [data]);
   return (
     <section className={`${styles.graph} animeLeft`}>
       <div className={styles.total}>
         <p>Acessos: {total}</p>
+      </div>
+      <div className={styles.graphs}>
+        <VictoryPie data={graph} />
       </div>
     </section>
   );
