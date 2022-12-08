7cb6b3e42f6596fbeee95d9e82d76e9c326597dd
pascoalkahamba <pascoalkahamba25@gmail.com>
2022-12-08 07:28:34 +0100

    utilizei a função STATS_GET no UserStats

--- src/Components/User/UserStats.jsx
+++ src/Components/User/UserStats.jsx
@@ -1,9 +1,17 @@
-import React from "react";
+import React, { useEffect } from "react";
 import Head from "../Helper/Head";
 import useFetch from "../../Hooks/useFetch";
+import { STATS_GET } from "../../api";
 
 const UserStats = () => {
   const { error, loading, request, data } = useFetch();
+  useEffect(() => {
+    async function getData() {
+      const { url, options } = STATS_GET();
+      await request(url, options);
+    }
+    getData();
+  }, [request]);
   return (
     <div>
       <Head title="Estatísticas" />
